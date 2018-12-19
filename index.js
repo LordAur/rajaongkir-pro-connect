147a2365e50859bedb719a906623d2890327e85a
Yudha Pratama <yudha.webdev@gmail.com>
2018-12-19 10:26:41 +0700

    Penambahan function get city

--- index.js
+++ index.js
@@ -1,17 +1,61 @@
 const unirest = require('unirest');
 
-exports.getProvince = (apiKey) => {
+exports.getProvince = (apiKey, provinceId = null) => {
   return new Promise((resolve, reject) => {
-    unirest
-      .get('https://pro.rajaongkir.com/api/province')
-      .headers({ key: apiKey })
-      .timeout(30000)
-      .end((response) => {
-        if (response.statusCode !== 200) {
-          reject(response.body);
-        }
+    if (provinceId === null) {
+      unirest
+        .get('https://pro.rajaongkir.com/api/province')
+        .headers({ key: apiKey })
+        .timeout(30000)
+        .end((response) => {
+          if (response.statusCode !== 200) {
+            reject(response.body);
+          }
 
-        resolve(response.body.rajaongkir.results);
-      });
+          resolve(response.body.rajaongkir.results);
+        });
+    } else {
+      unirest
+        .get(`https://pro.rajaongkir.com/api/province?id=${provinceId}`)
+        .headers({ key: apiKey })
+        .timeout(30000)
+        .end((response) => {
+          if (response.statusCode !== 200) {
+            reject(response.body);
+          }
+
+          resolve(response.body.rajaongkir.results);
+        });
+    }
+  });
+};
+
+exports.getCity = (apiKey, cityId = null, provinceId) => {
+  return new Promise((resolve, reject) => {
+    if (cityId === null) {
+      unirest
+        .get(`https://pro.rajaongkir.com/api/city?province=${provinceId}`)
+        .headers({ key: apiKey })
+        .timeout(30000)
+        .end((response) => {
+          if (response.statusCode !== 200) {
+            reject(response.body);
+          }
+
+          resolve(response.body.rajaongkir.results);
+        });
+    } else {
+      unirest
+        .get(`https://pro.rajaongkir.com/api/city?id=${cityId}&province=${provinceId}`)
+        .headers({ key: apiKey })
+        .timeout(30000)
+        .end((response) => {
+          if (response.statusCode !== 200) {
+            reject(response.body);
+          }
+
+          resolve(response.body.rajaongkir.results);
+        });
+    }
   });
 };
